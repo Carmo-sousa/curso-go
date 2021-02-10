3f0d132deee3ead12a3273336f8c21436b586222
Rômulo do Carmo Sousa <carmosousa20@outlook.com>
2021-02-09 21:19:54 -0400

    :sparkles:  Primeira aula do curso

---
+++ fundamentos/primeiro/primeiro.go
@@ -0,0 +1,22 @@
+package main
+
+// Importando um pacote go.
+import "fmt"
+
+/*
+Os códigos em Go são organizados em pacotes
+e para usá-los é necessário declarar um ou vários imports
+*/
+
+// Todo programa executável em go inicia na função main.
+func main() {
+	fmt.Println("Hello world!")
+}
+
+/*
+Sobre comentários...
+
+1 - Priorize código legível e faça comentários que agregam valor!
+2 - Evite comentários óbvios.
+3 - Durante o curso abuse dos comentários.
+*/
