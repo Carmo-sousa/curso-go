f8d7ceaec3720be83d37df84cb10dde69876c9ae
Rômulo do Carmo Sousa <carmosousa20@outlook.com>
2021-02-14 21:46:21 -0400

    :sparkles:  Aula 06
    Valores iniciais das variaveis

---
+++ fundamentos/zeros/zeros.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+  var a int
+  var b float64
+  var c bool
+  var d string
+  var e *int
+
+  fmt.Println("Tipos zeros em Go!")
+
+  fmt.Printf("%v %v %v %v %v\n", a, b, c, d, e)
+}
