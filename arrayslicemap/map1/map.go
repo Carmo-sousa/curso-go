8bcade2200e3d329890f7d2ab845cca79973bd0f
Rômulo do Carmo Sousa <carmosousa20@outlook.com>
2021-02-21 14:39:39 -0400

    Aula 27 - Map#1

---
+++ arrayslicemap/map1/map.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Map - dict[python]
+	// Maps devem ser inicializados!
+	// var aprovados map[int]string
+	aprovados := make(map[int]string)
+
+	aprovados[123456789] = "Mária"
+	aprovados[524524253] = "Pedro"
+	aprovados[781781349] = "Ana"
+
+	// fmt.Println(aprovados)
+
+	for cpf, nome := range aprovados {
+		fmt.Printf("CPF: %d\nNome: %s\n", cpf, nome)
+	}
+
+	// Acessando um valor
+  fmt.Println(aprovados[123456789])
+
+  // Deletando um valor
+  delete(aprovados, 781781349)
+  fmt.Println(aprovados)
+}
